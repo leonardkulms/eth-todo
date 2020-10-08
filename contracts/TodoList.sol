e09017cfcea49e17480fecd07966e8349f7918de
Leonard Kulms <leonard.kulms@icloud.com>
2020-10-08 16:03:38 +0200

    :sparkles: add backend function to create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,9 +3,20 @@ pragma solidity >=0.5.0;
 contract TodoList {
   uint public taskCount = 0;
 
+  constructor() public {
+    createTask("Create simple TodoList Example");
+  }
+
   struct Task {
     uint id;
     string description;
     bool completed;
+  }
+
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
   }
 }
