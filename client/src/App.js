4a176c1c6cc6a2956868daf24be6a4d004fbb744
Leonard Kulms <leonard.kulms@icloud.com>
2020-10-08 17:27:07 +0200

    :sparkles: connect to the local blockchain react

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Web3 from 'web3'
 import logo from './logo.svg';
 import './App.css';
 
-import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './todoList-config'
+import { TODO_LIST_ABI, TODO_LIST_ADDRESS } from './todoList-config';
 
 function App() {
+  const [ accounts, setAccounts ] = useState([]);
+  useEffect( () => loadBlockchainData(), []);
+  
+  const loadBlockchainData = async () => {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
+    const accs = await web3.eth.getAccounts();
+    setAccounts(accs);
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-       
+        <ul>
+          { accounts.map( a => <li> {a} </li> ) } 
+        </ul>
       </header>
     </div>
   );
